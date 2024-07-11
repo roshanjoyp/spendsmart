fce64c33d997bd9185584005ec7e301fc195af4d
roshanjoyp <68321507+roshanjoyp@users.noreply.github.com>
2024-07-11 19:14:18 +0530

    font Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:spendsmart/app/app.bottomsheets.dart';
@@ -36,6 +38,15 @@ class MainApp extends StatelessWidget {
             navigatorObservers: [
               StackedService.routeObserver,
             ],
+            theme: ThemeData(fontFamily: "ArchitypeRenner"),
+            scrollBehavior: const MaterialScrollBehavior().copyWith(
+              dragDevices: {
+                PointerDeviceKind.mouse,
+                PointerDeviceKind.touch,
+                PointerDeviceKind.stylus,
+                PointerDeviceKind.unknown
+              },
+            ),
           );
         });
   }
